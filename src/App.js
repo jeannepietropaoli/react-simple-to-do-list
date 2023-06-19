c4744961e208b06cca31435cbb907b008137073b
Jeanne <j.pietropaoli@yahoo.fr>
2023-06-19 21:43:12 +0200

    Add logic to manage current task selection and update

--- src/App.js
+++ src/App.js
@@ -5,27 +5,53 @@ import { nanoid } from "nanoid";
 
 function App() {
   const [notes, setNotes] = React.useState([])
+  const [currentNoteId, setCurrentNoteId] = React.useState(null)
 
   console.log(notes)
+
   function createNewNote() {
-    console.log("new note")
     setNotes(prevNotes => {
       return [...prevNotes, {
         title : `Note ${prevNotes.length + 1}`,
-        body : "",
-        id : nanoid()
+        body : "hello",
+        id : nanoid(),
+        selected : false
       }]
     })
   }
 
+  function findNoteIndex(noteId) {
+    const note = notes.filter(note => note.id === noteId)[0]
+    return notes.indexOf(note)
+  }
+
   function selectTask(noteId) {
+    setNotes(prevNotes => {
+      return prevNotes.map(prevNote => {
+        return prevNote.id === noteId
+          ? {...prevNote, selected : true}
+          : {...prevNote, selected : false}
+      })
+    })
+    setCurrentNoteId(noteId)
+  }
+
+  function editNote(e, noteId) {
+    console.log(e.target)
     console.log(noteId)
+    setNotes(prevNotes => {
+      return prevNotes.map(prevNote => {
+        return prevNote.id === noteId
+          ? {...prevNote, body : e.target.value}
+          : prevNote
+      })
+    })
   }
 
   return (
     <div className="app">
       <Sidebar createNewNote={createNewNote} selectTask={selectTask} notes={notes} />
-      <Editor />
+      <Editor notes={notes} editNote={editNote} currentNoteId={currentNoteId} />
     </div>
   );
 }
